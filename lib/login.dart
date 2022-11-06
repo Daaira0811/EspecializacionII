43772f65f1c0198cac4973142038e3370b244517
Tomas Valdes <tomasvicenteignacio@gmail.com>
2022-11-06 01:34:44 -0300

    Se sube login y register, se modifica main

---
+++ lib/login.dart
@@ -0,0 +1,182 @@
+
+
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'dart:convert';
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_fincet/register.dart';
+
+// aqui solo cambie el nombre de la clase login
+
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Login",
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+       
+        appBar: AppBar(
+         centerTitle: true,
+         backgroundColor: Color.fromARGB(255,96, 95, 95)  ,
+          
+          
+          title: Text("FINCET",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold
+          ),
+          ),
+        ),
+
+        body: cuerpo(context)
+           
+        );
+  }
+}
+
+Widget cuerpo(context){
+  return Container(
+    color: Color.fromARGB(255, 46, 46,46),
+    /*
+    decoration: BoxDecoration(
+      image: DecorationImage(image: NetworkImage("https://i.pinimg.com/originals/62/b8/36/62b836f9037d62691f11b41b5025f0b4.jpg"),
+      fit: BoxFit.cover
+      )
+     
+    ),
+    */
+    child: Center(
+      child: Column(
+        
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: 150,),
+          textoIniciarSesion(),
+
+          textoIngresarCorreo(),
+          campoUsuario(),
+          campoClave(),
+          SizedBox(height: 15,),
+         
+          Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+      
+            children: [         
+              botonEntrar(),
+              SizedBox(height: 15,),
+              botonRegistrar(context),
+            ],
+          ),
+
+        ],
+      ),
+      ),
+    );
+}
+
+Widget textoIniciarSesion(){
+  Row(children: [
+    
+  ],);
+  return Text("Iniciar Sesion", 
+  style: TextStyle(color: Colors.white, 
+  fontSize: 30,
+  fontWeight: FontWeight.bold),
+  );
+}
+
+Widget textoIngresarCorreo(){
+  return Text("Ingrese correo",
+   
+   style: TextStyle(color: Colors.white, 
+   fontSize: 15,
+   fontWeight: FontWeight.normal),
+  
+
+  );
+  
+}
+
+Widget campoUsuario(){
+  Text("Ingrese correo");
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
+    
+    child: TextField(
+      decoration: InputDecoration(
+        hintText: "Correo",
+        fillColor: Color.fromARGB(255, 217, 217, 217),
+        filled: true,
+        
+      ),
+    ),
+  );
+}
+
+Widget campoClave(){
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
+    child: TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "Clave",
+        fillColor: Color.fromARGB(255,  217, 217, 217),
+        filled: true,
+        
+      ),
+    ),
+  );
+}
+
+Widget botonEntrar(){
+  return TextButton(
+   style: TextButton.styleFrom(
+    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+    foregroundColor: Colors.black,
+    backgroundColor: Color.fromARGB(255, 217, 217, 217),
+   ),
+ 
+   onPressed: (){},
+   child: Text("Iniciar sesion",
+   style: TextStyle(fontSize: 25),)
+   );
+}
+
+Widget botonRegistrar(context){
+  return TextButton(
+   style: TextButton.styleFrom(
+    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+    foregroundColor: Colors.black,
+    backgroundColor: Color.fromARGB(255,217, 217, 217),
+
+   ),
+ 
+   onPressed: (){
+    Navigator.push(
+      context, 
+      MaterialPageRoute(builder: (context) => Registrarse(),));
+   },
+   child: Text("Registrarse",
+   style: TextStyle(fontSize: 25),)
+   );
+}
---
+++ lib/register.dart
@@ -0,0 +1,156 @@
+
+
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_fincet/login.dart';
+
+class Registrarse extends StatelessWidget {
+  const Registrarse({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+         centerTitle: true,
+         backgroundColor: Color.fromARGB(255,96, 95, 95)  ,
+         title: Text("FINCET",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold
+          ),
+          ),
+          
+        ),
+
+        body: cuerpo(context)
+           
+    );
+  }
+}
+ 
+
+Widget cuerpo(context){
+  return Container(
+    color: Color.fromARGB(255, 46, 46,46),
+    /*
+    decoration: BoxDecoration(
+      image: DecorationImage(image: NetworkImage("https://i.pinimg.com/originals/62/b8/36/62b836f9037d62691f11b41b5025f0b4.jpg"),
+      fit: BoxFit.cover
+      )
+     
+    ),
+    */
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: 150,),
+          signIn(),
+          campoUsuario(),
+          campoClave(),
+          campoReingresarClave(),
+          SizedBox(height: 15,),
+         
+          Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+      
+            children: [         
+              botonRegistrar(context),
+              SizedBox(height: 15,),
+              botonLogear(context),
+            ],
+          ),
+
+        ],
+      ),
+      ),
+    );
+}
+
+Widget signIn(){
+  return Text("Registrarse", 
+  style: TextStyle(color: Colors.white, 
+  fontSize: 30,
+  fontWeight: FontWeight.bold),);
+}
+
+Widget campoUsuario(){
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
+    child: TextField(
+      decoration: InputDecoration(
+        hintText: "Correo",
+        fillColor: Color.fromARGB(255, 217, 217, 217),
+        filled: true,
+        
+      ),
+    ),
+  );
+}
+
+Widget campoClave(){
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
+    child: TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "Clave",
+        fillColor: Color.fromARGB(255,  217, 217, 217),
+        filled: true,
+        
+      ),
+    ),
+  );
+}
+
+Widget campoReingresarClave(){
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
+    child: TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "Reingrese clave",
+        fillColor: Color.fromARGB(255,  217, 217, 217),
+        filled: true,
+        
+      ),
+    ),
+  );
+}
+
+Widget botonRegistrar(context){
+  return TextButton(
+   style: TextButton.styleFrom(
+    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+    foregroundColor: Colors.black,
+    backgroundColor: Color.fromARGB(255,217, 217, 217),
+   ),
+ 
+   onPressed: (){
+    Navigator.push(context,
+     MaterialPageRoute(builder: (context)=>Login()));
+   },
+   child: Text("Registrarse",
+   style: TextStyle(fontSize: 20),)
+   );
+}
+
+Widget botonLogear(context){
+  return TextButton(
+   style: TextButton.styleFrom(
+    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+    foregroundColor: Colors.black,
+    backgroundColor: Color.fromARGB(255,217, 217, 217) 
+   ),
+ 
+   onPressed: (){
+    Navigator.push(context,
+     MaterialPageRoute(builder: (context)=>Login()));
+   },
+   child: Text("Iniciar sesion",
+   style: TextStyle(fontSize: 20),)
+   );
+}
