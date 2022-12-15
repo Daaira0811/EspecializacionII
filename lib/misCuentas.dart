42a7b1c2e75bd7ef639069064adc9cfe0e90174b
Daaira0811 <73968061+Daaira0811@users.noreply.github.com>
2022-12-15 16:16:05 -0300

    Funcionalidad de cuentas casi-lista

--- lib/misCuentas.dart
+++ lib/misCuentas.dart
@@ -1,11 +1,14 @@
 //import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_fincet/BalanceGeneral.dart';
 import 'package:flutter_application_fincet/DAO/DB.dart';
 import 'package:flutter_application_fincet/login.dart';
 import 'package:flutter_application_fincet/models/Cuenta.dart';
 import 'package:flutter_application_fincet/widgets/navBar.dart';
 import 'package:flutter_application_fincet/widgets/sideMenu.dart';
+
+import 'models/Dinero.dart';
 
 class mostrarCuentas extends StatefulWidget {
   //final Data data;
@@ -15,7 +18,6 @@ class mostrarCuentas extends StatefulWidget {
   @override
   State<mostrarCuentas> createState() => _mostrarCuentasState();
 }
-
 
 class _mostrarCuentasState extends State<mostrarCuentas> {
   int index = 0;
@@ -86,11 +88,48 @@ Widget cuerpo(context) {
                           // if we got our data
                         } else if (snapshot.hasData) {
                           // Extracting data from snapshot object
-                          final List<Cuenta> data =
+                          final List<Cuenta> dataCuentas =
                               snapshot.data as List<Cuenta>;
                           return Center(
                               child: (Column(
-                            children: [misCuentas(context, data)],
+                            children: [
+                              FutureBuilder(
+                                  future: ListaDinero(),
+                                  builder: (ctx, snapshot) {
+                                    // Checking if future is resolved
+                                    if (snapshot.connectionState ==
+                                        ConnectionState.done) {
+                                      // If we got an error
+                                      if (snapshot.hasError) {
+                                        return Center(
+                                          child: Text(
+                                            '${snapshot.error} occurred',
+                                            style: TextStyle(fontSize: 18),
+                                          ),
+                                        );
+
+                                        // if we got our data
+                                      } else if (snapshot.hasData) {
+                                        // Extracting data from snapshot object
+                                        final List<Dinero> data =
+                                            snapshot.data as List<Dinero>;
+                                        return Center(
+                                            child: (Column(
+                                          children: [
+                                            misCuentas(
+                                                context, dataCuentas, data)
+                                          ],
+                                          //  ultimosMovimientos(context, data, cuentas)
+                                          // ultimosMovimientos(context, data)
+                                        )));
+                                      }
+                                    }
+                                    return Center(
+                                      child: CircularProgressIndicator(),
+                                    );
+                                  }),
+                              //  ultimosMovimientos(context),
+                            ],
                           )));
                         }
                       }
@@ -134,14 +173,26 @@ ListaCuentas() async {
   return list.toList();
 }
 
-Widget misCuentas(context, List<Cuenta> data) {
+String balancePorCuenta(List<Dinero> data, int index) {
+  int balance = 0;
+  data.forEach((element) {
+    if (int.parse(element.idCuenta.toString()) ==
+        index) if (element.tipoOperacion == 'esGasto') {
+      balance += int.parse(element.monto.toString()) * -1;
+    } else {
+      balance += int.parse(element.monto.toString());
+    }
+  });
+  print(balance);
+  return balance.toString();
+}
 
-
+Widget misCuentas(context, List<Cuenta> dataCuentas, List<Dinero> data) {
   return ListView.separated(
     shrinkWrap: true,
     primary: false,
     padding: const EdgeInsets.all(12),
-    itemCount: data.length,
+    itemCount: dataCuentas.length,
     itemBuilder: (BuildContext context, int index) {
       return Card(
         color: Colors.white,
@@ -168,7 +219,7 @@ Widget misCuentas(context, List<Cuenta> data) {
                   children: [
                     Padding(
                       padding: const EdgeInsets.only(top: 15),
-                      child: Text(data[index].nombreCuenta.toString(),
+                      child: Text(dataCuentas[index].nombreCuenta.toString(),
                           style: const TextStyle(
                               fontSize: 22, fontWeight: FontWeight.bold)),
                     )
@@ -180,12 +231,14 @@ Widget misCuentas(context, List<Cuenta> data) {
                     SizedBox(
                       height: 15,
                     ),
-                    Text("Balance",
-                        style: const TextStyle(fontSize: 18)),
+                    Text("Balance", style: const TextStyle(fontSize: 18)),
                     SizedBox(
                       height: 15,
                     ),
-                    Text(data[index].saldo.toString(),
+                    Text(
+                        balancePorCuenta(
+                                data, int.parse(dataCuentas[index].id.toString()))
+                            .toString(),
                         style: const TextStyle(
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
